bedd985d32b9563437938951126139a52d0b49eb
Yasir Notezai <79937476+yasirnotezai@users.noreply.github.com>
2024-05-01 23:37:44 +0300

    Delete part1.js

--- part1.js
+++
@@ -1,9 +0,0 @@
-function openURL() {
-    var url = document.getElementById('urlInput').value.trim();
-    if (url !== '') {
-        if (!url.startsWith('http://') && !url.startsWith('https://')) {
-            url = 'http://' + url; // adding http:// if not provided
-        }
-        window.open(url, '_blank');
-    }
-}
